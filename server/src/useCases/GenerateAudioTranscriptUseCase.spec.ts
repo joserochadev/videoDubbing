8b7ae35273fc89b27bbb05f571358a5fbcc47fe2
José Rocha <joserochadev@gmail.com>
2024-09-08 16:22:36 -0300

    test: generate audio transcription use case

--- server/src/useCases/GenerateAudioTranscriptUseCase.spec.ts
+++ server/src/useCases/GenerateAudioTranscriptUseCase.spec.ts
@@ -1,12 +1,32 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { GenerateAudioTranscriptUseCase } from './GenerateAudioTranscriptUseCase'
+import { TranscriptModule } from '@/modules/transcript/TranscriptModule'
 
 describe('Genetare audio transcript use case', () => {
-	it.only('should be able to generate transcription', async () => {
-		const sut = new GenerateAudioTranscriptUseCase()
+	it('should be able to generate transcription', async () => {
+		const transcriptModule = new TranscriptModule()
+		const sut = new GenerateAudioTranscriptUseCase(transcriptModule)
 
-		const audioPath = 'kkk'
+		const audioPath = 'output/audio/audio.mp3'
 
-		await sut.execute({ audioPath })
+		const transcriptMock = vi
+			.spyOn(transcriptModule, 'transcript')
+			.mockResolvedValueOnce('This is a sample transcript')
+
+		const { transcription } = await sut.execute({ audioPath })
+
+		// expect(transcriptMock).toHaveBeenCalledWith(audioPath)
+		expect(transcription).toBeTruthy()
+		expect(transcription.length).toBeGreaterThan(0)
+		expect(typeof transcription).toBe('string')
+	})
+
+	it('should not be able to generate transcription with wrong audio path', async () => {
+		const transcriptModule = new TranscriptModule()
+		const sut = new GenerateAudioTranscriptUseCase(transcriptModule)
+
+		const audioPath = 'wrong_audio_path'
+
+		await expect(() => sut.execute({ audioPath })).rejects.toBeInstanceOf(Error)
 	})
 })
